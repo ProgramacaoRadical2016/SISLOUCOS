ed4e1e25ef33bcba89d8a25885ed53f279641d49
Germano Manente Neto <gmanente@gmail.com>
2016-11-16 08:43:07 -0300

    Deletar

--- conexao.php
+++
@@ -1,4 +0,0 @@
-<?php
-	mysql_connect("localhost","root","admin") or die("Não foi possivel se conectar ao servidor");
-	mysql_select_db("sisclinic_novo") or die("Banco de dados não encontrado");
-?>
